bb46d2e4d950a9334ce9a723bb208b8a3a8e3400
mgraham8 <mgraham8@uw.edu>
2019-11-22 15:39:56 -0800

    final

--- app.js
+++ app.js
@@ -1,3 +1,36 @@
+/*
+ * Michael Graham
+ * 11/22/19
+ * Section AL - Tal Wolman
+ * HW4
+ *
+ * This API provides information on a selection of books.
+ * The API supports four endpoints, three of which can send both 400 and 500 error status codes
+ * The fourth endpoint only supports 500 error codes
+ *
+ * **Endpoints**
+ * /bestreads/description/:book_id
+ * Provides a description of the book corresponding to book_id
+ * Response type: text
+ * This endpoint can send errors with 400 or 500 status codes
+ *
+ * /bestreads/info/:book_id
+ * Provides title and author information on the book corresponding to the book_id
+ * Response type: JSON
+ * This endpoint can send errors with 400 or 500 status codes
+ *
+ * /bestreads/reviews/:book_id
+ * Provides a JSON object containing all reviews of the book corresponding to book_id
+ * Response type: JSON
+ * This endpoint can send errors with 400 or 500 status codes
+ *
+ * /bestreads/books
+ * Provides a JSON object containing title and id information on all books known to the API
+ * Response type: JSON
+ * This endpoint only supports errors with 500 status codes
+ *
+ */
+
 "use strict";
 
 const util = require('util');
@@ -9,53 +42,83 @@ const globPromise = util.promisify(glob);
 const app = express();
 
 const INVALID_REQUEST = 400;
+const INTERNAL_ERROR = 500;
 
 app.use(express.static('public'));
 
-//Endpoints
+/*
+ * This endpoint provides a description of the book corresponding to book_id.
+ * Response type is text
+ */
 app.get('/bestreads/description/:book_id', async (req,res) => {
   let book = req.params['book_id'].toLowerCase();
   try {
     let path = "books/" + book + "/description.txt"
     let content = await readFileAsync(path);
     res.type('text');
-    res.send(content);
+    if (content === INVALID_REQUEST) {
+      res.status(INVALID_REQUEST).send("No results found for " + book);
+    } else {
+      res.send(content);
+    }
   } catch (err) {
     res.type("text");
-    res.status(INVALID_REQUEST).send("(1) No results found for " + book)
+    res.status(INTERNAL_ERROR).send("Something went wrong on the server, try again later.");
   }
 });
 
+/*
+ * This endpoint provides title and author information on the book corresponding to the book_id
+ * Response type is JSON
+ */
 app.get('/bestreads/info/:book_id', async (req, res) => {
   let book = req.params['book_id'].toLowerCase();
   try {
     let path = "books/" + book + "/info.txt";
     let content = await getInfo(path);
-    res.json(content);
+    if (content === INVALID_REQUEST) {
+      res.type("text");
+      res.status(INVALID_REQUEST).send("No results found for " + book);
+    } else {
+      res.json(content);
+    }
   } catch (err) {
     res.type("text");
-    res.status(INVALID_REQUEST).send("(2) No results found for " + req.params['book_id']);
+    res.status(INTERNAL_ERROR).send("Something went wrong on the server, try again later.");
   }
 });
 
+/*
+ * This endpoint provides a JSON object containing all reviews of the book corresponding to book_id
+ * Response type is JSON
+ */
 app.get('/bestreads/reviews/:book_id', async (req, res) => {
   let book = req.params['book_id'].toLowerCase();
   let paths;
   let reviews = [];
   try {
     paths = await globPromise("books/" + book + "/review*.txt");
+    if (paths.length === 0) {
+      res.type("text");
+      res.status(INVALID_REQUEST).send("No results found for " + book);
+    } else {
     for (let i = 0; i < paths.length; i++) {
       let review = await buildReview(paths[i]);
       reviews.push(review);
-      console.log(reviews[i]);
     }
     res.json(reviews);
+  }
   } catch (err) {
     res.type("text");
-    res.status(INVALID_REQUEST).send("(3) No results found for " + req.params['book_id']);
+    res.status(INTERNAL_ERROR).send("Something went wrong on the server, try again later.");
   }
 });
 
+/*
+ * This endpoint provides a JSON object containing title and id information on all books
+ * known to the API
+ * Response type is JSON
+ */
 app.get('/bestreads/books', async (req, res) => {
   try {
     let result = [];
@@ -75,14 +138,25 @@ app.get('/bestreads/books', async (req, res) => {
     res.json(newJson);
   } catch (err) {
     res.type("text");
-    res.status(INVALID_REQUEST).send("(4) No results found");
+    res.status(INTERNAL_ERROR).send("Something went wrong on the server, try again later");
   }
 });
 
+/**
+ * This function splits the passed-in text by line, returning an array with each element
+ * being a separate line
+ * @param {Text} data - Text data being split-up
+ * @return {Array} Array with each element being a separate line
+ */
 function splitLines(data) {
   return data.split("\n");
 }
 
+/**
+ * This function gets a books title given its book_id
+ * @param {String} id - ID of the book thats title is being found
+ * @return {String} title - title of the passed-in book
+ */
 async function getTitle(id) {
   let path = "books/" + id + "/info.txt";
   let info = await readFileAsync(path);
@@ -90,45 +164,70 @@ async function getTitle(id) {
   return title;
 }
 
-//Functions
+/**
+ * This function reads the passed-in file asyncronously, returning a Promise value
+ * @param {String} path - Directory path of the file being read
+ * @return {Text} Value of the resolved promise object
+ */
 async function readFileAsync(path) {
   try {
     let content = await fs.readFile(path, 'utf8');
     return content;
   } catch (error) {
-    console.error(error);
+    if (error.code === "ENOENT") {
+      return INVALID_REQUEST;
+    }
   }
 }
 
+/**
+ * This function builds a JSON object containing the info read from the passed-in directory path
+ * @param {String} path - Directory path of a books info.txt file
+ * @return {Object} jsonInfo - JSON object with title and author information of a book
+ */
 async function getInfo(path) {
   let info = await readFileAsync(path);
-  let lines = info.split("\n");
+  if (info === INVALID_REQUEST) {
+    return info;
+  } else {
+    let lines = splitLines(info);
 
-  let title = lines[0];
-  let author = lines[1];
+    let title = lines[0];
+    let author = lines[1];
 
-  let jsonInfo = {
-    "title": title,
-    "author": author
+    let jsonInfo = {
+      "title": title,
+      "author": author
+    }
+    return jsonInfo;
   }
-  return jsonInfo;
 }
 
+/**
+ * This function builds a JSON object containing the data of a single book review
+ * @param {String} path - Directory path of a books review file
+ * @return {Object} newJson - JSON object containing the name, rating, and text information
+ * of a single book review
+ */
 async function buildReview(path) {
   let review = await readFileAsync(path);
-  let lines = review.split("\n");
+  if (review === INVALID_REQUEST) {
+    return review;
+  } else {
+    let lines = splitLines(review);
 
-  let name = lines[0];
-  let rating = lines[1];
-  let text = lines[2];
+    let name = lines[0];
+    let rating = lines[1];
+    let text = lines[2];
 
-  let newJson = {
-    "name": name,
-    "rating": rating,
-    "text": text
+    let newJson = {
+      "name": name,
+      "rating": rating,
+      "text": text
+    }
+
+    return newJson;
   }
-
-  return newJson;
 }
 
 const PORT = process.env.PORT || 8000;
--- public/bestreads.js
+++ public/bestreads.js
@@ -15,12 +15,11 @@
       .then(checkStatus)
       .then(response => response.json())
       .then(showBooks)
-      .catch(console.error("Error"));
+      .catch(handleError);
   }
 
   function showBooks(response) {
     let books = response.books;
-    console.log(books);
     for (let i = 0; i < books.length; i++) {
       let book = gen("div");
       let cover = gen("img");
@@ -28,10 +27,92 @@
       let title = gen("p");
       title.textContent = books[i].title;
 
+      book.classList.add("selectable");
+      cover.classList.add("selectable");
+      title.classList.add("selectable");
+
+      book.addEventListener("click", function() {
+        id("all-books").classList.add("hidden");
+        id("single-book").classList.remove("hidden");
+        fillContainer(books[i].book_id);
+      });
+      // cover.addEventListener("click", function() {
+      //   id("all-books").classList.add("hidden");
+      //   id("single-book").classList.remove("hidden");
+      //   fillContainer(books[i].book_id);
+      // });
+      // title.addEventListener("click", function() {
+      //   id("all-books").classList.add("hidden");
+      //   id("single-book").classList.remove("hidden");
+      //   fillContainer(books[i].book_id);
+      // });
+
       book.appendChild(cover);
       book.appendChild(title);
       id("all-books").appendChild(book);
     }
+  }
+
+  function fillContainer(book) {
+    fetchInfo(book);
+    fetchDesc(book);
+    fetchReviews(book);
+  }
+
+  function fetchInfo(book) {
+    fetch("/bestreads/info/" + book)
+      .then(checkStatus)
+      .then(resp => resp.json())
+      .then(showInfo)
+      .catch(handleError);
+  }
+
+  function showInfo(response) {
+    id("book-title").textContent = response.title;
+    id("book-author").textContent = response.author;
+  }
+
+  function fetchDesc(book) {
+    fetch("/bestreads/description/" + book)
+      .then(checkStatus)
+      .then(resp => resp.text())
+      .then(showDesc)
+      .catch(handleError);
+  }
+
+  function showDesc(response) {
+    id("book-description").textContent = response;
+  }
+
+  function fetchReviews(book) {
+    fetch("/bestreads/reviews/" + book)
+      .then(checkStatus)
+      .then(resp => resp.json())
+      .then(processReviews)
+      .catch(handleError);
+  }
+
+  function processReviews(response) {
+    let avgRating = 0;
+    let count = 0;
+    for (let i = 0; i < response.length; i++) {
+      let reviewer = gen("h3");
+      reviewer.textContent = response[i].name;
+      let rating = gen("h4");
+      rating.textContent = "Rating: " + response[i].rating;
+      let review = gen("p");
+      review.textContent = response[i].text;
+
+      id("book-reviews").appendChild(reviewer);
+      id("book-reviews").appendChild(rating);
+      id("book-reviews").appendChild(review);
+
+      avgRating += parseFloat(response[i].rating);
+      count ++;
+    }
+    avgRating = (avgRating / count);
+    avgRating = avgRating.toFixed(1);
+    id("book-rating").textContent = "" + avgRating;
   }
 
   /**
@@ -57,6 +138,12 @@ function checkStatus(response) {
   return response;
 }
 
+function handleError(response) {
+  id("book-data").classList.add("hidden");
+  id("error-text").classList.remove("hidden");
+  id("home").disabled = true;
+}
+
 /**
  * This helper funtion generates a new DOM element
  * @param {string} tagName - DOM element that is to be created
@@ -65,4 +152,4 @@ function checkStatus(response) {
 function gen(tagName) {
   return document.createElement(tagName);
 }
-});
+})();
